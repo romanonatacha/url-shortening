f1c8613b857bfe8bd87de2f09b8db1b7458dec6e
Natacha Romano <natacharomano@cvccorp.com.br>
2020-07-09 11:13:07 -0300

    Updates

---
+++ precache-manifest.74b00d2ca5e256288686f18f9402044a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d624de6b3ccd937c79755f457e5ac575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5446f0579caba9683be",
+    "url": "/static/css/main.08e84f8e.chunk.css"
+  },
+  {
+    "revision": "10dd6ac7e4e33b3e0911",
+    "url": "/static/js/2.66d1a1ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66d1a1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5446f0579caba9683be",
+    "url": "/static/js/main.800a1935.chunk.js"
+  },
+  {
+    "revision": "0db9fa8f26b3bd510d6f",
+    "url": "/static/js/runtime-main.a420526a.js"
+  }
+]);
